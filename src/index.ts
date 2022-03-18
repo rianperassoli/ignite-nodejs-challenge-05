7ca9d960bb548d277014c48561873ab9a30f3eec
Rian Perassoli <rian.perassoli@gmail.com>
2022-03-18 14:46:59 -0300

    Adicionado o swagger e criada a documentação

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(express.json());
 
 app.use("/users", usersRoutes);
