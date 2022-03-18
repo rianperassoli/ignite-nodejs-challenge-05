6eeb050a1e453773833f75bd6e05be0eb48a2506
Rian Perassoli <rian.perassoli@gmail.com>
2022-03-18 13:47:41 -0300

    Implementação dos métodos do desafio de solid

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,13 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailExists = this.usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new Error("E-mail already exists");
+    }
+
+    return this.usersRepository.create({ email, name });
   }
 }
 
